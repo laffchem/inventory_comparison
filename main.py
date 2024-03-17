77084ab6e5a5a11812f4aae333abe0490a3c196c
John Laffey <laffeychem@gmail.com>
2024-03-17 15:26:33 -0400

    change name of first input value & variables for clarity

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-second_oldest_file = input("Input the name of the older file. Copy and paste it...\n")
+second_newest_file = input("Input the name of the older file. Copy and paste it...\n")
 most_recent_file = input(
     "Input the name of the most recent file. Copy and paste it...\n"
 )
 
-sof_df = pd.read_csv(second_oldest_file)
+snf_df = pd.read_csv(second_newest_file)
 mrf_df = pd.read_csv(most_recent_file)
 
-sof_df.sort_values(by="UPC", inplace=True)
+snf_df.sort_values(by="UPC", inplace=True)
 mrf_df.sort_values(by="UPC", inplace=True)
 
-merged_df = pd.merge(sof_df, mrf_df, on="UPC", suffixes=("_sof", "_mrf"))
+merged_df = pd.merge(snf_df, mrf_df, on="UPC", suffixes=("_snf", "_mrf"))
 
 merged_df["QtyChange"] = merged_df["QtyAvailable_mrf"] - merged_df["QtyAvailable_sof"]
 
@@ -19,5 +19,5 @@ merged_df["ChangeStatus"] = "No Change"
 merged_df.loc[merged_df["QtyChange"] > 0, "ChangeStatus"] = "Quantity Increase"
 merged_df.loc[merged_df["QtyChange"] < 0, "ChangeStatus"] = "Quantity Decrease"
 
-final_df = merged_df[["UPC", "QtyAvailable_sof", "QtyAvailable_mrf", "ChangeStatus"]]
+final_df = merged_df[["UPC", "QtyAvailable_snf", "QtyAvailable_mrf", "ChangeStatus"]]
 final_df.to_csv("./inventory_comparisons/comparison.csv")
