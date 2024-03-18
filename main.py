8472fca05d78ffb37ea5cfa907da48abe7fa16b3
John Laffey <laffeychem@gmail.com>
2024-03-18 15:44:53 -0400

    Add change to filter out items with < 3 QtyAvailable in that column

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+pd.options.mode.copy_on_write = True  # This creates a copy in memory instead of providing a view. (Will be standard behavior in pandas 3.0)
+
 second_newest_file = input("Input the name of the older file. Copy and paste it...\n")
 most_recent_file = input(
     "Input the name of the most recent file. Copy and paste it...\n"
@@ -15,9 +17,16 @@ merged_df = pd.merge(snf_df, mrf_df, on="UPC", suffixes=("_snf", "_mrf"))
 
 merged_df["QtyChange"] = merged_df["QtyAvailable_mrf"] - merged_df["QtyAvailable_snf"]
 
-merged_df["ChangeStatus"] = "No Change"
-merged_df.loc[merged_df["QtyChange"] > 0, "ChangeStatus"] = "Quantity Increase"
-merged_df.loc[merged_df["QtyChange"] < 0, "ChangeStatus"] = "Quantity Decrease"
+filtered_df = merged_df[merged_df["QtyAvailable_mrf"] > 3]
 
-final_df = merged_df[["UPC", "QtyAvailable_snf", "QtyAvailable_mrf", "ChangeStatus"]]
-final_df.to_csv("./inventory_comparisons/comparison.csv")
+filtered_df["QtyChange"] = (
+    filtered_df["QtyAvailable_mrf"] - filtered_df["QtyAvailable_snf"]
+)
+
+filtered_df.loc[:, "ChangeStatus"] = "No Change"
+filtered_df.loc[filtered_df["QtyChange"] > 0, "ChangeStatus"] = "Quantity Increase"
+filtered_df.loc[filtered_df["QtyChange"] < 0, "ChangeStatus"] = "Quantity Decrease"
+
+final_df = filtered_df[["UPC", "QtyAvailable_snf", "QtyAvailable_mrf", "ChangeStatus"]]
+
+final_df.to_csv("./comparisons/comparison.csv")
